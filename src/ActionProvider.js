bee5a001472ea1c07b988155fe1892ccb933875b
Jonathan Kent <2466629k@student.gla.ac.uk>
2024-01-17 19:22:17 +0000

    Working with no loading

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -10,86 +10,12 @@ class ActionProvider {
     //this.createCustomMessage = createCustomMessage
     this.setState = setStateFunc;
   }
-
-  componentDidMount() {
-    this.getIP();
-    this.createUser();
-  }
-
-  getIP = async () => {
-    const res = await axios.get("https://api.ipify.org/?format=json");
-    //console.log(res.data);
-    localStorage.setItem("IP_Address", JSON.stringify(res.data.ip));
-    return res.data.ip;
-  };
-
-  createUser = async () => {
-    var ip_address = JSON.parse(localStorage.getItem("IP_Address"));
-    if (ip_address == null){
-      ip_address = await this.getIP()
-    }
-
-    fetch('http://130.209.243.228:3001/addusers', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ip_address}),
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        //console.log(data);
-      });
-  };
-
-  createResponse = async (question, response) => {
-    var time = Date();
-    var ip_address = JSON.parse(localStorage.getItem("IP_Address"));
-    if (ip_address == null){
-      ip_address = await this.getIP()
-    }
-
-    fetch('http://130.209.243.228:3001/addquestions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({time, question, response, ip_address}),
-    })
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        //console.log(data);
-      });
-  };
-
+    
   empty() {
-    const emptyMessage = this.createChatBotMessage("Hello, it appears you have not entered a message. Type in your questions about blood pressure to get information.")
+    const emptyMessage = this.createChatBotMessage("Hello, it appears you have not entered a message. Type in your questions about blood pressure to get information.", null, true)
     this.updateChatbotState(emptyMessage);
     //localStorage.removeItem("IP_Address");
   };
-
-  store_data(question, response){
-    var stored_data = JSON.parse(localStorage.getItem("Data"));
-    var ip_address = JSON.parse(localStorage.getItem("IP_Address"));
-    const json_data = {Time: Date(), Question: question, Response: response}
-
-    if (stored_data == null){
-      stored_data = []
-    }
-    stored_data.push(json_data)
-    //console.log(stored_data)
-    if (ip_address == null){
-      this.getIP();
-      this.createUser();
-    }
-    this.createResponse(question, response);
-
-    localStorage.setItem("Data", JSON.stringify(stored_data));
-  }
 
   handleRequest = (clicked) => {
     const question = this.createClientMessage(clicked)
@@ -99,17 +25,15 @@ class ActionProvider {
   handleNewMessage = (clicked) => {
     var message;
     if (Array.isArray(message_to_response[clicked])){
-      this.store_data(clicked, message_to_response[clicked][0])
       message = this.createChatBotMessage(
         message_to_response[clicked][0],
         {
           widget: "MoreInfo", payload: clicked
-        }
+        }, true
       );
     } else {
-      this.store_data(clicked, message_to_response[clicked])
       message = this.createChatBotMessage(
-        message_to_response[clicked],
+        message_to_response[clicked], null, true
       );
     }
 
@@ -117,9 +41,8 @@ class ActionProvider {
   }
 
   handleNewExtraMessage = (clicked) => {
-    this.store_data(clicked + " 1", message_to_response[clicked][1])
     const message = this.createChatBotMessage(
-      message_to_response[clicked][1],
+      message_to_response[clicked][1], null, true
     );
 
     this.updateChatbotState(message);
@@ -128,39 +51,36 @@ class ActionProvider {
   handleNewSummaryMessage = (response) => {
     
     var [new_response, new_extra] = this.format_message(response)
-    var message = this.createClientMessage(new_response)
-    // if (new_extra == "None"){
-    //   var message = this.createChatBotMessage(
-    //     new_response,
-    //   );
-    // } else {
-    //   var message = this.createChatBotMessage(
-    //     new_response, 
-    //     {
-    //       widget: "MoreInfoAPI", payload: new_extra
-    //     }
-    //   );
-    // }
+    if (new_extra == "None"){
+      var message = this.createChatBotMessage(
+        new_response
+      );
+    } else {
+      var message = this.createChatBotMessage(
+        new_response, 
+        {
+          widget: "MoreInfoAPI", payload: new_extra
+        }
+      );
+    }
 
     this.updateStreamState(message);
   }
 
   handleNewExtraSummaryMessage = (info_array) => {
-    this.store_data(info_array[0] + " 1", info_array[1])
     const message = this.createChatBotMessage(
-      info_array[1],
+      info_array[1], null, true
     );
 
     this.updateChatbotState(message);
   }
 
   handleNewWidgetMessage = (clicked, widget) => {
-    this.store_data(clicked, message_to_response[clicked])
     const message = this.createChatBotMessage(
       message_to_response[clicked],
       {
         widget: widget
-      }
+      }, true
     );
 
     this.updateChatbotState(message);
@@ -170,43 +90,40 @@ class ActionProvider {
     const message = this.createChatBotMessage(
       {
         widget: "ImageHandler", payload: image_name
-      }
+      }, true
     );
 
     this.updateChatbotState(message);
   }
 
   handleNewImageMessage = (clicked, image_name) => {
-    this.store_data(clicked, message_to_response[clicked])
     const message = this.createChatBotMessage(
       message_to_response[clicked],
       {
         widget: "ImageHandler", payload: image_name
-      }
+      }, true
     );
 
     this.updateChatbotState(message);
   }
 
   handleNewVideoMessage = (clicked, video_url) => {
-    this.store_data(clicked, message_to_response[clicked])
     const message = this.createChatBotMessage(
       message_to_response[clicked],
       {
         widget: "VideoHandler", payload: video_url
-      }
+      }, true
     );
 
     this.updateChatbotState(message);
   }
 
   handleNewHyperlinkMessage = (clicked, link) => {
-    this.store_data(clicked, message_to_response[clicked])
     const message = this.createChatBotMessage(
       message_to_response[clicked],
       {
         widget: "HyperlinkHandler", payload: link
-      }
+      }, true
     );
 
     this.updateChatbotState(message);
@@ -214,7 +131,7 @@ class ActionProvider {
 
   addMessage = (response) => {
     const message = this.createChatBotMessage(
-      response,
+      response, null, true
     );
 
     this.updateChatbotState(message);
@@ -222,7 +139,6 @@ class ActionProvider {
 
   handleOfficeBP = () => {
     var clicked = "Office BP"
-    this.store_data(clicked, message_to_response[clicked])
     for (let i = 0; i < message_to_response[clicked].length - 1; i++){
       this.addMessage(message_to_response[clicked][i])
     }
@@ -230,7 +146,7 @@ class ActionProvider {
       message_to_response[clicked][message_to_response[clicked].length - 1],
       {
         widget: "ImageHandler", payload: "office_bp"
-      }
+      }, true
     );
 
     this.updateChatbotState(message);
@@ -267,7 +183,7 @@ class ActionProvider {
       question["question"],
       {
         widget: "QuestionTemplate", payload: question
-      }
+      }, true
     );
 
     this.updateChatbotState(message)
@@ -298,7 +214,7 @@ class ActionProvider {
         next_question["question"],
         {
           widget: "QuestionTemplate", payload: next_question
-        }
+        }, true
       );
       this.updateChatbotState(message)
     }
